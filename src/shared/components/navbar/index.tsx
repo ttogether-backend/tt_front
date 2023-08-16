94b16653eb7f94def4eb4182a18da9ee40b618a9
거지 <jngmnj4@gmail.com>
2023-08-16 17:48:47 +0900

    [ADD] NavBar, Footer 추가

--- src/shared/components/navbar/index.tsx
+++ src/shared/components/navbar/index.tsx
@@ -1,5 +1,27 @@
+import Search from './search/Search';
+import NavUsers from './nav-users/NavUsers';
+import { Link } from 'react-router-dom';
+import styles from './NavBar.module.scss';
+import Nav from './nav/Nav';
+
 const Navbar = () => {
-  return <>Navbar</>;
+  return (
+    <header className={styles.header}>
+      <div className="container">
+        <div className={styles.hd_top}>
+          <div className={styles.hd_left}>
+            <Link to="/" className={styles.logo}>
+              <div className={styles.img}>트래블투게더</div>
+            </Link>
+            <Search />
+          </div>
+          <NavUsers />
+        </div>
+        <Nav />
+      </div>
+    </header>
+  );
 };
 
 export default Navbar;
+
