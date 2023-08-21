b8762ac87a68a197c872181cb574cea51664a08a
yurik1m <yuri.k.tech@gmail.com>
2023-08-21 17:08:39 +0900

    [MOD] - mocking을 위한 main.tsx import 수정

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { RecoilRoot } from 'recoil';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import App from './App.tsx';
+import { worker } from './mocks/browser'
 
 const queryClient = new QueryClient();
 
 //개발 모드일때만 사용하겠다
 if (process.env.NODE_ENV === 'development') {
-  const { worker } = require('./mocks/browser')
   worker.start()
 }
 
