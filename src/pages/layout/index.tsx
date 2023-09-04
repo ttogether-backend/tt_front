d00f6723665e9331092c075ebc183a69b03a6363
cynral <05bandge@gmail.com>
2023-09-04 23:22:24 +0900

    [ADD] Body 컴포넌트 임시 추가

--- src/pages/layout/index.tsx
+++ src/pages/layout/index.tsx
@@ -1,10 +1,12 @@
 import Footer from '../../shared/components/footer';
 import Navbar from '../../shared/components/navbar';
+import { Body } from './Body.tsx';
 
 const Layout = () => {
   return (
     <>
       <Navbar />
+      <Body />
       <Footer />
     </>
   );
