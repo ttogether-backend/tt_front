7134bc42fa873e4e2bde5625135b5c4336e672d6
cynral <05bandge@gmail.com>
2023-09-09 21:16:26 +0900

    [ADD] 경로 alias 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import * as path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +8,9 @@ export default defineConfig({
   build: {
     minify: false,
   },
+  resolve: {
+    alias: {
+      src: path.resolve(__dirname, './src')
+    }
+  }
 });
