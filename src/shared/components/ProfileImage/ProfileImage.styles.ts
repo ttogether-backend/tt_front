d64e717c7db3b8f3f7055d66785aac201b34333c
cynral <05bandge@gmail.com>
2023-09-16 21:47:06 +0900

    [ADD] #8 ProfileImage 컴포넌트 추가

---
+++ src/shared/components/ProfileImage/ProfileImage.styles.ts
@@ -0,0 +1,54 @@
+import { ProfileImagePropsType } from './ProfileImage.types.ts';
+import styled from '@emotion/styled';
+import { css } from '@emotion/react';
+
+const profileImageSizeCss = {
+  '17.03': css({
+    width: '17.03px',
+    height: '17.03px'
+  }),
+  '21.28': css({
+    width: '21.28px',
+    height: '21.28px'
+  }),
+  '24': css({
+    width: '24px',
+    height: '24px'
+  }),
+  '27': css({
+    width: '27px',
+    height: '27px'
+  }),
+  '32': css({
+    width: '32px',
+    height: '32px'
+  }),
+  '34.06': css({
+    width: '34.06px',
+    height: '34.06px'
+  }),
+  '36': css({
+    width: '36px',
+    height: '36px'
+  }),
+  '40': css({
+    width: '40px',
+    height: '40px'
+  }),
+  '46': css({
+    width: '46px',
+    height: '46px'
+  }),
+  '64': css({
+    width: '64px',
+    height: '64px'
+  }),
+  '160': css({
+    width: '160px',
+    height: '160px'
+  })
+}
+export const ProfileImageStyle = styled.div<ProfileImagePropsType>`
+   background-image: ${(props) => `url(${props.src})`};
+  ${(props) => profileImageSizeCss[props.imageSize]};
+`
---
+++ src/shared/components/ProfileImage/ProfileImage.types.ts
@@ -0,0 +1,18 @@
+export type ProfileImageSize =
+  | '17.03'
+  | '21.28'
+  | '24'
+  | '27'
+  | '32'
+  | '34.06'
+  | '36'
+  | '40'
+  | '46'
+  | '64'
+  | '160'
+
+export interface ProfileImagePropsType {
+  src: String | ArrayBuffer | null | undefined;
+  // hasBorder: boolean;
+  imageSize: ProfileImageSize;
+}
