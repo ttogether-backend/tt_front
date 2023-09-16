b1f1810b08503434ab1ee572a196705d3c894032
cynral <05bandge@gmail.com>
2023-09-16 21:49:21 +0900

    [ADD] #8 ProfileImage 컴포넌트 테스트 코드 추가

--- src/pages/layout/Body.tsx
+++ src/pages/layout/Body.tsx
@@ -1,8 +1,19 @@
-import { FC } from 'react';
-// import { CustomDatepicker } from '../../shared/components/Datepicker/Datepicker.tsx';
+import { FC/*, useState*/ } from 'react';
+// import { CustomDatepicker } from 'src/shared/components/Datepicker/Datepicker.tsx';
+// import { ProfileImage } from 'src/shared/components/ProfileImage/ProfileImage.tsx';
 
 export const Body:FC = () => {
+
+  // const [srcState, setSrcState] = useState<String | ArrayBuffer | null>();
+  // const onFileChange = () => {
+  //   // @ts-ignore
+  //   const inputFile:File = document.getElementById("myfileinput").files[0];
+  //   setSrcState(URL.createObjectURL(inputFile));
+  // }
+  // @ts-ignore
   return <div>
     {/*<CustomDatepicker/>*/}
+    {/*<input id={'myfileinput'} type={'file'} onChange={onFileChange}></input>*/}
+    {/*<ProfileImage src={srcState} imageSize={'27'} />*/}
   </div>
 }
