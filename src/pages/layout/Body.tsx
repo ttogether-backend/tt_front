4a8123b71b34ed95b6b408ee114790bc182cbc83
cynral <05bandge@gmail.com>
2023-09-17 10:58:05 +0900

    [ADD] #8 ProfileSquare 테스트용 이미지와 코드 추가

--- src/pages/layout/Body.tsx
+++ src/pages/layout/Body.tsx
@@ -1,14 +1,9 @@
 import { FC } from 'react';
-import { CustomDatepicker } from 'src/shared/components/Datepicker/Datepicker.tsx';
-import { ProfileSquare } from '../../shared/components/ProfileSqure/ProfileSquare.tsx';
+import { Test1 } from 'src/pages/layout/Test1.tsx';
 
 export const Body:FC = () => {
 
-  // @ts-ignore
   return <div>
-    <CustomDatepicker/>
-    <ProfileSquare profileCount={1} src={['src/shared/components/ProfileSqure/img.png']} />
-    <ProfileSquare profileCount={2} src={['src/shared/components/ProfileSqure/Component1.svg', 'src/shared/components/ProfileSqure/Ellipse498.svg']}/>
-    <ProfileSquare profileCount={3} src={['src/shared/components/ProfileSqure/Component1.svg', 'src/shared/components/ProfileSqure/Ellipse498.svg', 'src/shared/components/ProfileSqure/img.png']}/>
+    <Test1/>
   </div>
 }
