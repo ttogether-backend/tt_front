077dc986cfcbf3c80117e67ccf3ddafe754c0757
ujin_jeong <ujinjeong55@gmail.com>
2023-09-22 21:25:55 +0900

    [ADD] #11 개인정보처리방침 페이지 추가

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -4,6 +4,7 @@ import AccompanyModifyPage from 'src/pages/accompany/AccompanyModifyPage';
 import AccompanyDetailPage from 'src/pages/accompany/AccompanyDetailPage';
 import AccompanyListPage from 'src/pages/accompany/AccompanyListPage';
 import { Test1 } from 'src/pages/layout/Test1';
+import { PrivacyPolicy } from '../pages/policy/PrivacyPolicy.tsx';
 
 const Router = () => {
   return (
@@ -14,6 +15,7 @@ const Router = () => {
         <Route path="/accompany/modify" element={<AccompanyModifyPage />} />
         <Route path="/accompany/:id" element={<AccompanyDetailPage />} />
         <Route path="/accompany/" element={<AccompanyListPage />} />
+        <Route path="/privacy-policy" element={<PrivacyPolicy />} />
         <Route path="/test" element={<Test1 />} />
       </Routes>
     </BrowserRouter>
