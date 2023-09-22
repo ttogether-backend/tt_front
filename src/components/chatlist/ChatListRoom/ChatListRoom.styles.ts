4f90979182a27dbd648954b0c6d0d518a1847a94
ujin_jeong <ujinjeong55@gmail.com>
2023-09-22 20:45:48 +0900

    [ADD] 채팅 목록 채팅룸 컴포넌트 완성
    - ChatListRoom의 메시지 부분 추가
    - ChatListRoom의 동행 포스트 부분 추가

--- src/components/chatlist/ChatListRoom/ChatListRoom.styles.ts
+++ src/components/chatlist/ChatListRoom/ChatListRoom.styles.ts
@@ -5,24 +5,30 @@ export const ChatListRoomContainer = styled.div`
   width: 326.93px;
   height: 107.24px;
   padding: 13.62px;
-  grid-template-areas:
-    'profile chat-room-header'
-    'profile chat-room-body'
-    'profile accompany-post';
+  grid-template-areas:'profile chat-room';
   grid-template-columns: fit-content(100%) 1fr;
   column-gap: 6.81px;
-  row-gap: 8px;
 `;
 export const ChatProfileContainer = styled.div`
   grid-area: profile;
   width: 34.1px;
 `;
+export const ChatRoom = styled.div`
+  display: grid;
+  grid-area: chat-room;
+  grid-template-areas:
+          'header'
+          'body'
+          'accompany';
+`
 export const ChatHeader = styled.div`
   display: grid;
-  grid-area: chat-room-header;
+  height: fit-content(100%);
+  grid-area: header;
   grid-template-areas: 'title member-count time';
+  grid-template-columns: fit-content(100%) 1fr fit-content(100%);
 `;
-export const ChatRoomTile = styled.div`
+export const ChatRoomTitle = styled.div`
   grid-area: title;
   color: var(--bw-bk, #000);
   font-size: 12px;
@@ -49,4 +55,49 @@ export const ChatTime = styled.div`
   font-weight: 500;
   line-height: 16px;
   grid-area: time;
+  margin-left: auto;
 `;
+export const ChatBody = styled.div`
+  display: grid;
+  grid-area: body;
+  grid-template-areas: 'message alarm';
+  grid-template-columns: fit-content(100%) 1fr;
+`
+export const ChatMessage = styled.div`
+  grid-area: message;
+  width: calc(100% - 42px);
+  font-size: 10px;
+  font-weight: 500;
+  line-height: 16px;
+  letter-spacing: 0em;
+  text-align: left;
+  color: #52564E;
+  height: 32px;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  display: -webkit-box;
+  -webkit-line-clamp: 2;
+  -webkit-box-orient: vertical;
+`
+export const ChatAlarm = styled.div`
+  grid-area: alarm;
+  margin-left: auto;
+`
+export const AccompanyPost = styled.div`
+  display: grid;
+  grid-template-areas: 'accompany-status accompany-title';
+  column-gap: 8px;
+  grid-template-columns: fit-content(100%) 1fr;
+  margin-top: auto;
+`
+export const AccompanyStatus = styled.div`
+  grid-area: accompany-status;
+`
+export const AccompanyTitle = styled.div`
+  grid-area: accompany-title;
+  font-size: 11px;
+  font-weight: 600;
+  line-height: 24px;
+  text-align: left;
+  vertical-align: middle;
+`
--- src/components/chatlist/ChatListRoom/ChatListRoom.tsx
+++ src/components/chatlist/ChatListRoom/ChatListRoom.tsx
@@ -1,31 +1,36 @@
 import { FC } from 'react';
-// import Badges from '../../Badges';
 import * as Style from 'src/components/chatlist/ChatListRoom/ChatListRoom.styles';
 import { ChatListRoomPropsType } from './ChatListRoom.types';
 import { ProfileSquare } from 'src/shared/components/ProfileSqure/ProfileSquare';
+import { ChatCountBadge, ChatStateBadge } from 'src/shared/components/Badges/Badges.style.tsx';
 
 export const ChatListRoom: FC<ChatListRoomPropsType> = (props) => {
-  const { chatRoom, profileProps, /*accompanyPost,*/ messageInfo } = props;
+  const { chatRoom, profileProps, accompanyPost, messageInfo } = props;
   return (
     <Style.ChatListRoomContainer>
       <Style.ChatProfileContainer>
         <ProfileSquare {...profileProps} />
       </Style.ChatProfileContainer>
-      <Style.ChatHeader>
-        <Style.ChatRoomTile>{chatRoom.title}</Style.ChatRoomTile>
-        <Style.ChatRoomMemberCount>{chatRoom.memberCount}</Style.ChatRoomMemberCount>
-        <Style.ChatTime>{messageInfo.time}</Style.ChatTime>
-      </Style.ChatHeader>
-      <div
-        style={{
-          gridArea: 'chat-room-body',
-        }}
-      ></div>
-      <div
-        style={{
-          gridArea: 'accompany-post',
-        }}
-      ></div>
+      <Style.ChatRoom>
+        <Style.ChatHeader>
+          <Style.ChatRoomTitle>{chatRoom.title}</Style.ChatRoomTitle>
+          <Style.ChatRoomMemberCount>{chatRoom.memberCount}</Style.ChatRoomMemberCount>
+          <Style.ChatTime>{messageInfo.time}</Style.ChatTime>
+        </Style.ChatHeader>
+        <Style.ChatBody>
+          <Style.ChatMessage>{messageInfo.message}</Style.ChatMessage>
+          <Style.ChatAlarm>
+            <ChatCountBadge subType={""} text={chatRoom.messageCount.toString()}/>
+          </Style.ChatAlarm>
+        </Style.ChatBody>
+        <Style.AccompanyPost>
+          <Style.AccompanyStatus>
+            <ChatStateBadge subType={""} text={accompanyPost.accompanyStatus}/>
+          </Style.AccompanyStatus>
+          <Style.AccompanyTitle>{accompanyPost.title}</Style.AccompanyTitle>
+        </Style.AccompanyPost>
+      </Style.ChatRoom>
+
     </Style.ChatListRoomContainer>
   );
 };
