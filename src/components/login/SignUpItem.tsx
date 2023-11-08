027c201e76b8dc793d1bc0fa7b125db9f336f567
gyeonghyun kim <gyeonghyun.kimm@gmail.com>
2023-11-08 18:50:14 +0900

    [add] 이메일 인증 메일 전송

--- src/components/login/SignUpItem.tsx
+++ src/components/login/SignUpItem.tsx
@@ -74,6 +74,22 @@ export const SignUpItemDiv = styled.div`
       color: #acacb3;
     }
   }
+
+  .validItemIput {
+	font-family: Pretendard Variable;
+	width: 70%;
+    height: 49px;
+    border-radius: 5px;
+    border: 1px solid;
+    border-color: #d9d9d9;
+    :focus {
+      outline: none;
+    }
+
+    .placeholder {
+      color: #acacb3;
+    }
+  }
 `;
 
 type loginType = {
@@ -86,30 +102,35 @@ type loginType = {
   termsAgreementList: (number | boolean)[];
 };
 
-axios({
-	method: 'get',
-	url: 'http://ec2-44-203-114-252.compute-1.amazonaws.com:8000/api/v1/members/register/terms'
-})
-.then((response) => { console.log(response.data) })
-.catch((Error) => { console.log(Error) });
+// axios({
+// 	method: 'get',
+// 	url: 'http://ec2-44-203-114-252.compute-1.amazonaws.com:8000/api/v1/members/register/terms'
+// })
+// .then((response) => { console.log(response.data) })
+// .catch((Error) => { console.log(Error) });
 
-const ValidateEmail = (email) => {
-	// axios({
-	// 	method: 'post',
-	// 	url: 'http://ec2-44-203-114-252.compute-1.amazonaws.com:8000/api/v1/members/code/verify',
-	// 	data: {
-	// 		// email:
-
-	// 	}
-	// });
+const SendValidationEmail = (email) => {
+	
 	console.log(email);
+	axios({
+		method: 'post',
+		url: 'http://ec2-44-203-114-252.compute-1.amazonaws.com:8000/api/v1/members/code/issue',
+		data: {
+			email: email
+	 	}
+	 })
+	 .then((response) => { console.log(response.data) })
+	 .catch((Error) => { console.log(Error) });
+	const el = document.getElementById('validation');
+	el.style.display = 'inline';
 }
 
 const SignUpItem = () => {
   const {
     register, // input field 지정
     handleSubmit,
-	errors,
+	formState: { errors },
+	trigger,
 	getValues
   } = useForm<loginType>();
 
@@ -117,6 +138,8 @@ const SignUpItem = () => {
     console.log(data);
   };
   
+//   console.log("errors", errors);
+
   return (
     <>
 	<SignUpTitle>
@@ -131,20 +154,40 @@ const SignUpItem = () => {
 					placeholder="닉네임을 입력해주세요."
 					{...register('nickname', {required: true})}
 				/>
+				{errors.nickname && <p>닉네임은 필수입니다.</p>}
 			</SignUpItemDiv>
 			<SignUpItemDiv>
 				<span className="itemName">이메일</span>
 				<input 
 					className="itemIput"
-					placeholder="이메일을 입력해주세요."
-					type="email"
-					pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
-					{...register('loginId', {required: true})}
+					placeholder="이메일을 입력해주세요."{...register('loginId', {required: true,
+						pattern: /^[a-zA-Z0-9.\-+_]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/ 
+					})}
 				/>
+				{errors.loginId && <p>이메일 형식으로 입력해주세요.</p>}
 				<button className="validButton"
 					type="button"
-					onClick={ValidateEmail(getValues("loginId"))}>이메일 인증</button>
+					onClick= {async () => {
+						const valid = await trigger("loginId")
+						console.log("valid = " + valid)
+						if (valid) {
+							const email = getValues("loginId")
+							await SendValidationEmail(email)
+						}
+					}}>이메일 인증</button>
 			</SignUpItemDiv>
+			<div id='validation' style={{display:'none'}}>
+				<SignUpItemDiv>
+					<span 
+						className="itemName">인증번호</span>
+					<input
+						className="validItemIput"
+						{...register('code')}
+					/>
+					<button className="validButton"
+						type="button">인증</button>
+				</SignUpItemDiv>
+			</div>
 			<SignUpItemDiv>
 				<span className="itemName">비밀번호</span>
 				<input 
@@ -153,6 +196,7 @@ const SignUpItem = () => {
 					type="password"
 					{...register('password', {required: true})}
 				/>
+				{errors.password && <p>비밀번호는 필수입니다.</p>}
 			</SignUpItemDiv>
 			<SignUpItemDiv>
 				<span className="itemName">비밀번호 확인</span>
@@ -162,6 +206,7 @@ const SignUpItem = () => {
 					type="password"
 					{...register('passwordConfirm', {required: true})}
 				/>
+				{errors.passwordConfirm && <p>비밀번호 확인은 필수입니다.</p>}
 			</SignUpItemDiv>
 			<SignUpItemDiv>
 				<span className="itemName">이름</span>
@@ -170,15 +215,17 @@ const SignUpItem = () => {
 					placeholder="이름을 입력해주세요."
 					{...register('name', {required: true})}
 				/>
+				{errors.name && <p>이름은 필수입니다.</p>}
 			</SignUpItemDiv>
 			<SignUpItemDiv>
 				<span className="itemName">휴대폰번호</span>
 				<input 
 					className="itemIput"
 					placeholder="000-0000-0000 형식으로 입력해주세요."
-					pattern="[0-9]{3}-[0-9]{4}-[0-9]{4}"
+					pattern="^[0-9]{3}-[0-9]{4}-[0-9]{4}$"
 					{...register('phoneNumber', {required: true})}
 				/>
+				{errors.phoneNumber && <p>휴대폰번호는 필수입니다.</p>}
 			</SignUpItemDiv>
 			<SignUpItemDiv>
 				<input className="submitButton" type="submit" />
