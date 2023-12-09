911a2bb5fe42f05dc6ec114f5eadd826906d0bb8
gyeonghyun kim <gyeonghyun.kimm@gmail.com>
2023-12-10 02:41:03 +0900

    [ADD] 소셜 로그인 버튼 추가

--- src/components/login/LoginItem.style.tsx
+++ src/components/login/LoginItem.style.tsx
@@ -1,68 +1,130 @@
 import styled from '@emotion/styled';
 
 export const LoginItemWrap = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 454.5px;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	width: 454.5px;
+	margin-bottom: 3rem;
 
-  .submitButton {
-    width: 100%;
-    height: 50px;
-    margin-top: 10.73px;
-    background-color: #64c243;
-    border: transparent;
-    border-radius: 10px;
-    color: white;
-    font-weight: bold;
-    font-family: Pretendard Variable;
-  }
+	.submitButton {
+		width: 100%;
+		height: 50px;
+		margin-top: 10.73px;
+		background-color: #64c243;
+		border: transparent;
+		border-radius: 10px;
+		color: white;
+		font-weight: bold;
+		font-family: Pretendard Variable;
+	}
 
   
 `;
 
+
+
 export const LoginItemDiv = styled.div`
-  font-family: Pretendard Variable;
-  width: 454.5px;
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
+	font-family: Pretendard Variable;
+	width: 400px;
+	display: flex;
+	flex-direction: column;
+	justify-content: flex-start;
+	
+	.error-text {
+		font-size: 14px;
+		margin-top: 8px;
+		color: red;
+	}
 
-  .itemName {
-    margin-top: 2rem;
-    margin-bottom: 1rem;
-    font-size: 18px;
-    font-weight: bold;
-	text-align: left; 
-  }
-
-  .error-text {
-    font-size: 14px;
-    margin-top: 8px;
-    color: red;
-  }
-
-  .itemIput {
-    font-family: Pretendard Variable;
-    height: 49px;
-    border-radius: 5px;
-    border: 1px solid;
-    border-color: #d9d9d9;
-    :focus {
-      outline: none;
-    }
+	.itemIput {
+		font-family: Pretendard Variable;
+		height: 49px;
+		border-radius: 10px;
+		border: 1px solid;
+		border-color: #d9d9d9;
+		padding-left: 10px;
+		:focus {
+			outline: none;
+		}
+	}
 
     .placeholder {
-      color: #acacb3;
+		margin: 1rem;
+		color: #acacb3;
     }
+	
+	.kakaoLogin {
+		width: 100%;
+		height: 50px;
+		margin-top: 10.73px;
+		background-color: #FDE50A;
+		border: transparent;
+		border-radius: 10px;
+		color: #392020;
+		font-weight: bold;
+		font-family: Pretendard Variable;
+	}
+
+	.naverLogin {
+		width: 100%;
+		height: 50px;
+		margin-top: 10.73px;
+		background-color: #5DC563;
+		border: transparent;
+		border-radius: 10px;
+		color: #392020;
+		font-weight: bold;
+		font-family: Pretendard Variable;
+	}
+
+	.googleLogin {
+		width: 100%;
+		height: 50px;
+		margin-top: 10.73px;
+		background-color: #FFFFFF;
+		border: 1px solid #E5E8EB;
+		border-radius: 10px;
+		color: #392020;
+		font-weight: bold;
+		font-family: Pretendard Variable;
+	}
+
+	.hr-sect {
+        display: flex;
+        flex-basis: 100%;
+        align-items: center;
+        color: #43444A;
+        font-size: 16px;
+        margin: 8px 0px;
+      }
+      .hr-sect::before,
+      .hr-sect::after {
+        content: "";
+        flex-grow: 1;
+        background: rgba(0, 0, 0, 0.35);
+        height: 1px;
+        font-size: 0px;
+        line-height: 0px;
+        margin: 0px 16px;
+      }
   }
 `;
 
+
+
 export const LoginTitle = styled.div`
-  margin-bottom: 50px;
-  color: #000;
-  font-size: 32px;
-  font-weight: 700;
-  line-height: 150%;
-  font-family: Pretendard;
+	margin-bottom: 50px;
+	color: #000;
+	font-size: 32px;
+	font-weight: 700;
+	line-height: 150%;
+	font-family: Pretendard;
+`;
+
+export const ExtraButton = styled.div`
+	margin-top: 1.5rem;
+	margin-bottom: 1.5rem;
+	display: flex;
+	justify-content: space-between;
 `;
--- src/components/login/LoginItem.tsx
+++ src/components/login/LoginItem.tsx
@@ -6,9 +6,9 @@ import { ButtonStyleType } from '../../shared/components/ButtonsText/ButtonsText
 import { useForm, SubmitHandler } from 'react-hook-form';
 import axios from 'axios';
 import { Cookies } from 'react-cookie';
-import { LoginTitle, LoginItemDiv, LoginItemWrap } from './LoginItem.style';
+import { LoginTitle, LoginItemDiv, LoginItemWrap, ExtraButton } from './LoginItem.style';
 import { onLoginSuccess, onSilentRefresh } from "./Utils/LoginUtils";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, Link } from 'react-router-dom';
 
 type loginType = {
 	email: string,
@@ -19,74 +19,93 @@ type loginType = {
 const LoginItem = () => {
 	const navigate = useNavigate();
 
-	
-  const {
-    register, // input field 지정
-    handleSubmit,
-	formState: { errors },
-  } = useForm<loginType>();
 
-  const onSubmit = (data) => {
-    console.log(data);
+	const {
+		register, // input field 지정
+		handleSubmit,
+		formState: { errors },
+	} = useForm<loginType>();
 
-	axios.post('/api/v1/members/login/email', {
-		email : data.email,
-		password : data.password
-	})
-	.then((res) => {
-		console.log(res)
-		if (res.data.success) {
-			// console.log("accessToken:", accessToken, "refreshToken:", refreshToken);
-			onLoginSuccess(res.data);
-			navigate('/');
-		}
-	})
-	.catch((error) => {
-		console.log(error)
-		document.getElementById("signinFailureMessage").style.display = 'inline';
-	});
-  };
+	const onSubmit = (data) => {
+		console.log(data);
 
-  return (
-    <>
-	<LoginTitle>
-		로그인
-	</LoginTitle>
-	<LoginItemWrap>
-		<form onSubmit={handleSubmit(onSubmit)}>
-			<LoginItemDiv>
-				<span className="itemName">아이디</span>
-				<input 
-					className="itemIput"
-					placeholder="로그인 아이디"
-					{...register('email', { required: true,
-						pattern: /^[a-zA-Z0-9.\-+_]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/
-					})}
-				/>
-				{errors.email && <p className="error-text">이메일 형식으로 입력해주세요.</p>}
-			</LoginItemDiv>
-			<LoginItemDiv>
-				<span className="itemName">비밀번호</span>
-				<input 
-					className="itemIput"
-					placeholder="비밀번호"
-					type="password"
-					{...register('password', { required: true })}
-				/>
-				{errors.password && <p className="error-text">비밀번호를 입력해주세요.</p>}
-			</LoginItemDiv>
-			<LoginItemDiv>
-				<div id="signinFailureMessage" style = {{display:'none'}}>
-						<p>아이디와 비밀번호가 일치하지 않습니다.</p>
-				</div>
-			</LoginItemDiv>
-			<LoginItemDiv>
-				<button className="submitButton" type="submit">로그인</button>
-			</LoginItemDiv>
-		</form>
-	</LoginItemWrap>
-    </>
-  );
+		axios.post('/api/v1/members/login/email', {
+			email: data.email,
+			password: data.password
+		})
+			.then((res) => {
+				console.log(res)
+				if (res.data.success) {
+					// console.log("accessToken:", accessToken, "refreshToken:", refreshToken);
+					onLoginSuccess(res.data);
+					navigate('/');
+				}
+			})
+			.catch((error) => {
+				console.log(error)
+				document.getElementById("signinFailureMessage").style.display = 'inline';
+			});
+	};
+
+	return (
+		<>
+			<LoginTitle>
+				로그인
+			</LoginTitle>
+			<LoginItemWrap>
+				<form onSubmit={handleSubmit(onSubmit)}>
+					<LoginItemDiv>
+						<input
+							className="itemIput"
+							placeholder="아이디"
+							{...register('email', {
+								required: true,
+								pattern: /^[a-zA-Z0-9.\-+_]+@[a-zA-Z0-9.\-]+\.[a-zA-Z]{2,}$/
+							})}
+						/>
+						{errors.email && <p className="error-text">이메일 형식으로 입력해주세요.</p>}
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<input
+							className="itemIput"
+							placeholder="비밀번호"
+							type="password"
+							{...register('password', { required: true })}
+						/>
+						{errors.password && <p className="error-text">비밀번호를 입력해주세요.</p>}
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<div id="signinFailureMessage" style={{ display: 'none' }}>
+							<p className="error-text">아이디 또는 비밀번호가 맞지 않습니다.</p>
+						</div>
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<button className="submitButton" type="submit">로그인</button>
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<ExtraButton>
+							<div>아이디/비밀번호 찾기</div>
+							<div><Link to="/signup">회원가입</Link></div>
+						</ExtraButton>
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<div className="hr-sect">
+							간편 로그인
+						</div>
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<button className="kakaoLogin">카카오톡으로 로그인</button>
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<button className="naverLogin">네이버로 로그인</button>
+					</LoginItemDiv>
+					<LoginItemDiv>
+						<button className="googleLogin">구글로 로그인</button>
+					</LoginItemDiv>
+				</form>
+			</LoginItemWrap>
+		</>
+	);
 };
 
 export default LoginItem;
